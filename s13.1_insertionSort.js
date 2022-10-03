7a462201b9ed66e1fc5bde5df73058b7191e7408
Rafael Neto <rafaelneto.g@gmail.com>
2022-10-03 15:49:41 -0300

    INSERTION SORT IMPLEMENTED

--- s13.1_insertionSort.js
+++ s13.1_insertionSort.js
@@ -1,20 +1,12 @@
-function swap(arr, index1, index2) {
-  const temp = arr[index1];
-  arr[index1] = arr[index2];
-  arr[index2] = temp;
-}
-
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     const el = arr[i];
-    for (let j = i - 1; j > -1; j--) {
-      console.log(el, arr[j]);
-      if (el < arr[j] && (j === 0 || el > arr[j - 1])) {
-        swap(arr, i, j);
-        break;
-      }
+    let j = i - 1;
+    while (j > -1 && arr[j] > el) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    console.log(arr);
+    arr[j + 1] = el;
   }
   return arr;
 }
