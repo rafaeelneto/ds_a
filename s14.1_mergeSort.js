a89a4368af0f8355c6543b63893dfc93ce8e3dc2
Rafael Neto <rafaelneto.g@gmail.com>
2022-10-13 16:51:15 -0300

    merge sort implemented

--- s14.1_mergeSort.js
+++ s14.1_mergeSort.js
@@ -24,27 +24,41 @@ function mergePseudoCode(arr, arr2) {
   let j = 0;
 
   while (i < arr.length && j < arr2.length) {
-    if (arr[i] < arr[j]) {
+    if (arr[i] < arr2[j]) {
       sortedArray.push(arr[i]);
       i++;
-    } else if (arr[i] > arr[j]) {
-      sortedArray.push(arr2[j]);
-      j++;
     } else {
-      sortedArray.push(arr[i], arr2[j]);
-      i++;
+      sortedArray.push(arr2[j]);
       j++;
     }
   }
 
-  if (i === arr.length) {
+  if (i < arr.length) {
     sortedArray.push(...arr.slice(i));
-  } else if (j === arr2.length) {
+  } else if (j < arr2.length) {
     sortedArray.push(...arr2.slice(j));
   }
 
   return sortedArray;
 }
 
+function mergeSort(unsortedArray) {
+  // check if unsortedArray is smaller or equal to one
+  const uArrayLength = unsortedArray.length;
+  if (uArrayLength <= 1) {
+    return unsortedArray;
+  }
+
+  const arrayHalfEl = Math.floor(uArrayLength / 2);
+
+  const firstHalf = unsortedArray.slice(0, arrayHalfEl);
+  const lastHalf = unsortedArray.slice(arrayHalfEl, uArrayLength);
+
+  return mergePseudoCode(mergeSort(firstHalf), mergeSort(lastHalf));
+}
+
 // console.log(mergeTrial([1, 10, 50], [2, 14, 99, 100]));
-console.log(mergePseudoCode([1, 10, 50], [2, 14, 99, 100]));
+// console.log(mergePseudoCode([4], [6]));
+
+console.log(mergeSort([3, 1, 7, 10, 4, 6]));
+console.log(mergeSort([6, 2, 12, 5, 1, 87, 3, 54, 23, 5, 32, 34]));
